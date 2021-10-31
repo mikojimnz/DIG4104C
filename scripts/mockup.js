1755469e877d6f3529adee591b9186c78f404abc
Miko Jimenez <jimenezmikog@gmail.com>
2021-10-30 20:28:55 -0400

    Update mockup page

--- scripts/mockup.js
+++ scripts/mockup.js
@@ -1,10 +1,17 @@
 
 /* Show/Hide mobile alert banner */
+
 $(window).on('resize', function(){
     var win = $(this);
     if (win.width() < 768) { 
         $("#mobile-alert").addClass("show");
+        $("#mobile-alert").css("display", "block");
     } else {
         $("#mobile-alert").removeClass("show");
+        $("#mobile-alert").css("display", "none");
     }
 });
+
+window.dispatchEvent(new Event('resize'));
+
+gsap.fromTo(".proto-wrapper", {opacity: 0, y: 100}, {opacity: 1, y: 0, duration: 1.5});
